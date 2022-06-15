57a6fc5deaa7586d68f065defa94ac3dd672dc04
AzureAD\YuriFaria <yuri.faria@gamasti.com.br>
2022-06-15 15:33:03 -0300

    Página de "Detalhe" funcionando e mostrando o objeto clicado

--- AppShopping/AppShopping/ViewModels/StoresViewModel.cs
+++ AppShopping/AppShopping/ViewModels/StoresViewModel.cs
@@ -8,6 +8,7 @@ using System.Windows.Input;
 using Xamarin.Forms;
 using AppShopping.Libraries.Enums;
 using AppShopping.Libraries.Helpers.MVVM;
+using Newtonsoft.Json;
 
 namespace AppShopping.ViewModels
 {
@@ -30,9 +31,12 @@ namespace AppShopping.ViewModels
         
         public List<Establishment> _allEstablishments;
 
+        public ICommand DetailCommand { get; set; }
+
         public StoresViewModel()
         {
             SearchCommand = new Command(Search);
+            DetailCommand = new Command<Establishment>(Detail);
 
             var allEstablishments = new EstablishmentServices().GetEstablishment();
             var allStores = allEstablishments.Where(a=>a.Type == EstablishmentType.Store).ToList();
@@ -43,6 +47,13 @@ namespace AppShopping.ViewModels
         {
             //Lógica de Filtrar a lista de Lojas
             Establishments = _allEstablishments.Where(a => a.Name.ToLower().Contains(SearchWord.ToLower())).ToList();
+        }
+        private void Detail(Establishment establishment)
+        {
+            string establishmentSerialized = JsonConvert.SerializeObject(establishment);
+
+            //Shell > GoTo > EstablishmentDetail (como o objeto recebido)
+            Shell.Current.GoToAsync($"establishment/detail?establishmentSerialized={Uri.EscapeDataString(establishmentSerialized)}");
         }
     }
 }
